aeb9f6ff3b5a502ef5bf739de307ba9bd4004e82
atskyline <atskyline@gmail.com>
2018-11-05 21:57:23 +0800

    chg edit_uri

--- gen_site.py
+++ gen_site.py
@@ -8,9 +8,6 @@ site_dir = os.path.abspath(os.path.join(__file__, '../../atskyline.github.io'))
 nav_file_path = os.path.abspath(os.path.join(__file__, '../nav.yml'))
 site_name = "朱易辰的个人网站"
 
-print(docs_dir)
-print(site_dir)
-
 config = mkdocs.config.load_config(
     config_file = open(nav_file_path, "rb"),
     site_name = site_name,
@@ -18,6 +15,7 @@ config = mkdocs.config.load_config(
     site_url ='http://www.atskyline.com',
     repo_url = 'https://github.com/atskyline/atskyline.docs',
     repo_name = 'GitHub',
+    edit_uri='blob/master/',
     docs_dir = docs_dir,
     site_dir = site_dir
 )
